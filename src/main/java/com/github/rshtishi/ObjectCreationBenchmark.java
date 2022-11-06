83b26392c1c439aa41dff07e45594feb6b6cd5a7
rshtishi <rando.shtishi@gmail.com>
2022-11-06 14:20:56 +0100

    adding black hole example

---
+++ src/main/java/com/github/rshtishi/ObjectCreationBenchmark.java
@@ -0,0 +1,33 @@
+package com.github.rshtishi;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.concurrent.TimeUnit;
+
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode(Mode.AverageTime)
+@Fork(value = 1, jvmArgs = {"-Xms2G", "-Xmx2G"})
+public class ObjectCreationBenchmark {
+
+    @Benchmark
+    public void testDoNothing() {
+    }
+
+    @Benchmark
+    public void testObjectCreation(){
+        new Object();
+    }
+
+    @Benchmark
+    public Object testObjectCreationThatReturnsObject(){
+        return new Object();
+    }
+
+    @Benchmark
+    public void testObjectCreationWithBlackHole(Blackhole blackHole){
+        blackHole.consume(new Object());
+    }
+
+
+}
